c18e663a5360e7572ac12af7b1b67d08f5e930dc
Matthew Suhay <matthew.suhay@gmail.com>
2026-03-18 18:52:49 -0500

    Fixing react-native import

--- tsup.native.config.ts
+++ tsup.native.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'react-native',
-  format: ['cjs'], // Build for commonJS and ESmodules
+  format: ['cjs', 'esm'], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: false,
